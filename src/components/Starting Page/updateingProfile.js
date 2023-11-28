7fb2cdf764d92504c5f6a71d3ace2ab9ee6d1599
tanmaysihare <tanmaysihare@gmail.com>
2023-11-28 17:09:14 +0530

    task 4 without get request

--- src/components/Starting Page/updateingProfile.js
+++ src/components/Starting Page/updateingProfile.js
@@ -1,13 +1,12 @@
-import { useRef , useContext, useEffect, useState } from "react";
+import { useRef , useContext } from "react";
 import AuthContext from "../../store/auth-context";
-//import { useHistory } from "react-router-dom";
+import { useHistory } from "react-router-dom";
 
 const UpdatingProfile = () => {
     const authCtx = useContext(AuthContext);
-  //  const history = useHistory();
+    const history = useHistory();
   const displayNameRef = useRef();
   const photoUrlRef = useRef();
-  const [userData, setUserData] = useState(null);
 
   const submitHandler = async (event) => {
     event.preventDefault();
@@ -45,34 +44,10 @@ const UpdatingProfile = () => {
       // Handle successful update, if needed
       authCtx.setProfileCompleteState(true);
       console.log("Profile updated successfully:", data);
-    //  history.push('/home');
+      history.push('/home');
     } catch (error) {
       // Handle error, display a message, or redirect the user
       console.error("Profile update error:", error.message);
-    }
-  };
-  useEffect(() => {
-    if(authCtx.profileComplete){
-      fetchData();
-    }
-  },[authCtx.profileComplete]);
-
-  const fetchData = async () => {
-    try {
-      const url = `https://identitytoolkit.googleapis.com/v1/accounts:lookup?key=AIzaSyA-ZiBDqAYaaBy2czSnBwxdUgrRk0Y0Qjs/${authCtx.token}`;
-      const response = await fetch(url,{
-       
-        headers: {
-          "Content-Type":"application/json", 
-        },
-      });
-      if (!response.ok){
-        throw new Error("failed to fetch user data");
-      }
-      const data = await response.json();
-      setUserData(data);
-    }catch(error){
-      console.error('error fetching user data', error.message);
     }
   };
 
@@ -84,19 +59,11 @@ const UpdatingProfile = () => {
       <form onSubmit={submitHandler}>
         <h2>Contact Details</h2>
         <label>Full Name: </label>
-        <input type="text" required ref={displayNameRef} />
+        <input type="text" value={displayNameRef} required ref={displayNameRef} />
         <label>Profile Photo URL</label>
-        <input type="url" required ref={photoUrlRef} />
+        <input type="url" value={photoUrlRef} required ref={photoUrlRef} />
         <button type="submit">Update</button>
       </form>
-      {userData && (
-        <div>
-          <h2>User Data</h2>
-          <p>Display Name: {userData.displayName}</p>
-          <p>Photo URL: {userData.photoUrl}</p>
-          {/* Add more fields as needed */}
-        </div>
-      )}
     </>
   );
 };
